9980d089f6cfbf05bd6ea68a5b4ae91ea6460bd0
Lemokami <jesswinw@gmail.com>
2021-06-30 23:38:31 +0530

    fix loading issue

--- components/LoadingPage.tsx
+++ components/LoadingPage.tsx
@@ -4,10 +4,12 @@ import styled from 'styled-components';
 const icon = {
   hidden: {
     pathLength: 0,
+    opacity: 0,
     fill: 'rgba(0,0,0,0)',
   },
   show: {
     pathLength: 1,
+    opacity: 1,
     fill: 'rgba(0,0,0,1)',
     transition: {
       duration: 2,
