a7bcbf6793388f8be65312f2c396d7cc16dc3332
lemokami <jesswinw@gmail.com>
2021-09-18 12:51:27 +0530

    removed the sfx

--- components/SocialIcon/SocialIcon.tsx
+++ components/SocialIcon/SocialIcon.tsx
@@ -1,17 +1,8 @@
 import { SocialIconType, StyledSocialIcon } from './SocialIcon.styles';
-import { Howl } from 'howler';
 
 const SocialIcon: React.FC<SocialIconType> = ({ url, children, name }) => {
-  const click = new Howl({
-    src: ['/sounds/click.wav'],
-    volume: 0.4,
-  });
-
-  function handleMouse() {
-    click.play();
-  }
   return (
-    <StyledSocialIcon href={url} aria-label={name} onMouseEnter={handleMouse}>
+    <StyledSocialIcon href={url} aria-label={name}>
       {children}
     </StyledSocialIcon>
   );
